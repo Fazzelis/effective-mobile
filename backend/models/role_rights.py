522059004dbd5faa0e293d6a56e6d74b9e9d8968
Fazzelis <odidk08@gmail.com>
2025-12-30 00:55:09 +0700

    Изменены поля у role-rights

--- backend/models/role_rights.py
+++ backend/models/role_rights.py
@@ -8,9 +8,10 @@ class RoleRights(Base):
     __tablename__ = "role_rights"
     id = Column(UUID(as_uuid=True), primary_key=True, server_default=func.gen_random_uuid())
 
-    read_access = Column(Boolean)
-    write_access = Column(Boolean)
-    delete_access = Column(Boolean)
+    read_posts_access = Column(Boolean)
+    write_posts_access = Column(Boolean)
+    delete_posts_access = Column(Boolean)
+    manage_roles_access = Column(Boolean)
     role_id = Column(UUID(as_uuid=True), ForeignKey("role.id"))
 
     role = relationship("Role", back_populates="rights")
