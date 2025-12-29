868387a3dc5c2f0a779c2691fe41de7068122605
Fazzelis <odidk08@gmail.com>
2025-12-30 02:32:05 +0700

    Удалена таблица role_rights, изменена таблица role

---
+++ backend/models/__init__.py
@@ -0,0 +1,3 @@
+from .post import Post
+from .role import Role
+from .user import User
--- backend/models/role.py
+++ backend/models/role.py
@@ -9,7 +9,9 @@ class Role(Base):
     id = Column(UUID(as_uuid=True), primary_key=True, server_default=func.gen_random_uuid())
 
     name = Column(String)
-    rights_id = Column(UUID(as_uuid=True), ForeignKey("role_rights.id"))
+    read_posts_access = Column(Boolean)
+    write_posts_access = Column(Boolean)
+    delete_posts_access = Column(Boolean)
+    manage_roles_access = Column(Boolean)
 
     users = relationship("User", back_populates="role")
-    rights = relationship("RoleRights", back_populates="role")
